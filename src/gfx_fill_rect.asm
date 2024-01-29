f70ca632b74f3bbe2d99fa4df67112d719a4d96b
fourstix <fourstix@gmail.com>
2024-01-29 15:01:31 -0500

    Removed unused include statements

--- src/gfx_fill_rect.asm
+++ src/gfx_fill_rect.asm
@@ -8,9 +8,6 @@
 ; Copyright 2012 by Adafruit Industries
 ; Please see https://learn.adafruit.com/adafruit-gfx-graphics-library for more info
 ;-------------------------------------------------------------------------------
-
-#include    ../include/bios.inc
-#include    ../include/kernel.inc
 #include    ../include/ops.inc
 #include    ../include/gfx_display.inc
 #include    ../include/gfx_def.inc  
